cde72b1fc78434f7f84d052fdcfaadbcec4e797c
abul kalam <bd.abulkalam800@gamil.com>
2025-06-30 16:34:01 +0300

    adede card payment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const stripe = require("stripe")(process.env.PAYMENT_GATWAY);
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { default: Stripe } = require("stripe");
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 // Middlewares
@@ -10,7 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.icyvogv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,31 +22,84 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  
-}})
-
+  },
+});
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const db = client.db('zap-shift');
-    const parcelCollection = db.collection('parcel')
+    const db = client.db("zap-shift");
+    const parcelCollection = db.collection("parcel");
+    //  get api
 
-    app.post('/parcels', async (req, res) => {
-  try {
-    const parcel = req.body;
-    const result = await parcelCollection.insertOne(parcel);
-    res.send(result)
-  
-   
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+    app.get("/parcels", async (req, res) => {
+      try {
+        const userEamil = req.query.email;
+        const query = userEamil
+          ? {
+              created_by: userEamil,
+            }
+          : {};
+        const options = {
+          sort: { createdAt: -1 },
+        };
+
+        // Match the correct field: created_by_email
+        const result = await parcelCollection.find(query, options).toArray();
+
+        res.send(result);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    });
+
+    // single specefic api
+
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const specefic = { _id: new ObjectId(id) };
+      const result = await parcelCollection.findOne(specefic);
+      res.send(result);
+    });
+    app.post("/create-payment-intent", async (req, res) => {
+      const amountIncens = req.body.amountIncens;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amountIncens,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
+    // post Api
+    app.post("/parcels", async (req, res) => {
+      try {
+        const parcel = req.body;
+        const result = await parcelCollection.insertOne(parcel);
+        res.send(result);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    });
+    // delet api
+
+    app.delete("/parcels/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const result = await parcelCollection.deleteOne({
+          _id: new ObjectId(id),
+        });
+
+        res.send(result);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -52,16 +107,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
 // GET: Retrieve all parcels
-app.get('/',(req,res)=>{
-    res.send('Parcel server is rouning')
-})
+app.get("/", (req, res) => {
+  res.send("Parcel server is rouning");
+});
 
 // Server listen
 app.listen(port, () => {
